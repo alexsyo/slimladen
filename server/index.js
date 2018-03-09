6b09198793db03ecdd015b1ebeba7a0082aeafdb
Alessandro Stea <alex_ok@hotmail.it>
2018-03-09 20:39:32 +0100

    readded log

--- server/index.js
+++ server/index.js
@@ -11,22 +11,38 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send("abc"));
 app.post('/', (req, res) => {
-	console.log('-----------')
+	console.log('--------')
 	console.log(req.body)
 	res.send(req.body)
 })
 
-app.get('/session', (req, res) => session(res))
+app.get('/session', (req, res) => {
+	const now = moment().utc().add(1, 'minute').format();
+	console.log(now)
+	session(mock_schedule(now), res)
+})
+
+
+const mock_schedule = now => ({
+    "chargingschedule_periods": [
+        {
+            "max_current": "16",
+            "start_period": "0"
+        },
+        {
+            "max_current": "0",
+            "start_period": "1"
+        }
+    ],
+    "duration": "60",
+    "start_schedule": now
+});
 
 
 app.listen(port);
 
 
-const session = (res) => {
-	const now = moment().utc().add(2, 'minute').format();
-
-	console.log(now)
-
+const session = (schedule, res) => {
 	request({
 		method: 'POST',
 		headers: {
@@ -37,21 +53,8 @@ const session = (res) => {
 		json: {
 		    "contract_id": "NLTS3C0000001",
 		    "evse_id": "NL-EVN-E32486-13208",
-		    "schedule": {
-		        "chargingschedule_periods": [
-		            {
-		                "max_current": "16",
-		                "start_period": "0"
-		            },
-		            {
-		                "max_current": "0",
-		                "start_period": "20"
-		            }
-		        ],
-		        "duration": "60",
-		        "start_schedule": now
-		    },
-		    "subscription_id": "20581",
+		    "schedule": schedule,
+		    "subscription_id": "20582",
 		    "tariff_type": "D1"
 		}
 	}, (err, res, body) => console.log(res.statusCode + ' ' + body))

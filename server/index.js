6aeecc6759e12942e94d17bb7cafcc4b5d979e0f
Alessandro Stea <alex_ok@hotmail.it>
2018-03-09 19:45:54 +0100

    post a charging sessions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const Express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const moment = require('moment');
 
 
 const app = Express();
@@ -9,36 +10,52 @@ const port = 8000;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send("abc"));
-app.post('/', (req, res) => res.send(req.body))
+app.post('/', (req, res) => {
+	console.log('-----------')
+	console.log(req.body)
+	res.send(req.body)
+})
 
-
-
-
-// app.use('/abc', (req, res) => {
-// 	request.post('http://elaad-pp.driivz.com/api/ndr',
-// 	{
-// 		"operator": "NL-ELA",
-// 		"subscription_id" : "JmO6uPb-U=1Kg7",
-// 		[{
-// 		 "evse_id": "evseId2",
-// 		 "connector_no": 1,
-// 		 "contract_id":"NL-TNM-023232-X",
-// 		 "timestamp":"2014-11-11T12:56Z",
-// 		 "event" : {
-// 		 "type" : "session-started",
-// 		 "start_datetime":"2014-11-11T12:55Z"
-// 		}]
-// 	})
-// 	.on('response', response => res.send(response))
-
-// 	res.send("asfw")
-// });
-
-
-
-
-
+app.get('/session', (req, res) => session(res))
 
 
 app.listen(port);
+
+
+const session = (res) => {
+	const now = moment().utc().add(2, 'minute').format();
+
+	console.log(now)
+
+	request({
+		method: 'POST',
+		headers: {
+			'Authorization': 'Basic c21hcnRfY2hhcmdlMzpQZTUxaHV0N2tt',
+			'Content-Type': 'application/json'
+		},
+		url: 'https://elaad-pp.driivz.com/externalIncoming/secured/ocpi/smartcharge',
+		json: {
+		    "contract_id": "NLTS3C0000001",
+		    "evse_id": "NL-EVN-E32486-13208",
+		    "schedule": {
+		        "chargingschedule_periods": [
+		            {
+		                "max_current": "16",
+		                "start_period": "0"
+		            },
+		            {
+		                "max_current": "0",
+		                "start_period": "20"
+		            }
+		        ],
+		        "duration": "60",
+		        "start_schedule": now
+		    },
+		    "subscription_id": "20581",
+		    "tariff_type": "D1"
+		}
+	}, (err, res, body) => console.log(res.statusCode + ' ' + body))
+
+	res.send('ok');
+}
 

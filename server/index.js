625e27d50b66eaf073bbf0096c4cd534b1a3ac8d
Alessandro Stea <alex_ok@hotmail.it>
2018-03-09 16:29:57 +0100

    clean up

--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 const Express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+
 
 const app = Express();
-const port = 8080;
+const port = 8000;
 
-app.use('/', (req, res) => res.send("abc"));
-app.use('/abc', (req, res) => res.send("asfw"));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send("abc"));
+app.post('/', (req, res) => res.send(req.body))
+
+
+
+
+// app.use('/abc', (req, res) => {
+// 	request.post('http://elaad-pp.driivz.com/api/ndr',
+// 	{
+// 		"operator": "NL-ELA",
+// 		"subscription_id" : "JmO6uPb-U=1Kg7",
+// 		[{
+// 		 "evse_id": "evseId2",
+// 		 "connector_no": 1,
+// 		 "contract_id":"NL-TNM-023232-X",
+// 		 "timestamp":"2014-11-11T12:56Z",
+// 		 "event" : {
+// 		 "type" : "session-started",
+// 		 "start_datetime":"2014-11-11T12:55Z"
+// 		}]
+// 	})
+// 	.on('response', response => res.send(response))
+
+// 	res.send("asfw")
+// });
+
+
+
+
+
+
 
 app.listen(port);
 

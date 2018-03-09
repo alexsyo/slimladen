a2a16c25afadbdde4a5d76d076a964f3ac7dc04f
Alessandro Stea <alex_ok@hotmail.it>
2018-03-09 15:09:49 +0100

    new end point

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const Express = require('express');
 const app = Express();
 const port = 3000;
 
+app.use('/', (req, res) => res.send("abc"));
 app.use('/abc', (req, res) => res.send("asfw"));
 
 app.listen(port);

74a11b60ffc075a25e68b3af53b6ee28916a6485
Alessandro Stea <alex_ok@hotmail.it>
2018-03-09 15:24:42 +0100

    port changed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const Express = require('express');
 
 const app = Express();
-const port = 3000;
+const port = 8080;
 
 app.use('/', (req, res) => res.send("abc"));
 app.use('/abc', (req, res) => res.send("asfw"));

dde7aafd958ddc91f192264223f715adba13c189
Giuseppe Procaccianti <giuseppe.procaccianti@gmail.com>
2018-03-10 11:38:58 +0100

    - update plots with legends - parse location and calculate plan accordingly

--- R/get_charge_plan.R
+++ R/get_charge_plan.R
@@ -18,20 +18,35 @@ get_charge_plan <- function(location, soc)
   forecasts <- get_solar_forecasts(province, date, city)
   
   start_time <- Sys.time()
-  duration <- 8*60*60
+  
+  if(location=="home") duration <- 8*60*60
+    else if (location=="work") duration <- 6*60*60
+      else if (location=="out") duration <- 2*60*60
   
   chargingschedule_periods <- forecasts %>%
     filter(datetimeFC > start_time) %>%
-    arrange(desc(forecast)) %>%
-    mutate(max_current = as.character(c(rep(1,ptus_to_charge), rep(0, n()-ptus_to_charge)))) %>%
+      filter(datetimeFC < start_time + duration)
+  
+  time_available <- nrow(chargingschedule_periods)
+  
+  chargingschedule_periods %<>%
+    arrange(desc(forecast))
+  
+  if(time_available>ptus_to_charge) 
+    chargingschedule_periods %<>%
+      mutate(max_current = as.character(c(rep(1,ptus_to_charge), rep(0, n()-ptus_to_charge))))
+  else
+  chargingschedule_periods %<>% 
+    mutate(max_current = as.character(1))
+  
+  chargingschedule_periods %<>% 
     arrange(datetimeFC) %>%
     mutate(start_period = as.character(seq(0, length.out=n(), by=900))) %>%
     select(max_current, start_period)
   
-    
   schedule <- list(chargingschedule_periods = chargingschedule_periods, 
                    duration=as.character(duration), 
                    start_schedule = format(start_time, "%Y-%m-%dT%H:%M:%S%z"))
-  #toJSON(schedule, auto_unbox = TRUE, pretty=TRUE)
+  
   schedule
 }
